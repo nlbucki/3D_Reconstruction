b380476351f90579a9c7f1251c7e094095a339c9
Mike Danielczuk <mjd3@princeton.edu>
2018-05-08 22:25:57 -0700

    fixed switched images and masks in dataset

--- globgalob.py
+++ globgalob.py
@@ -14,11 +14,19 @@ folders = glob.glob(os.path.join(base_folder, '*'))
 for f in folders:
     if os.path.isdir(f):
 		folder_name = os.path.basename(f)
-		pics = glob.glob(os.path.join(f,'*.jpg'))
-		for i,p in enumerate(pics):
-			# print os.path.basename(p)
-			os.rename(p, os.path.join(os.path.dirname(p), folder_name+'_'+str(i)+'_input.jpg'))
+		# pics = glob.glob(os.path.join(f,'*.jpg'))
+		# for i,p in enumerate(pics):
+		# 	# print os.path.basename(p)
+		# 	os.rename(p, os.path.join(os.path.dirname(p), folder_name+'_'+str(i)+'_input.jpg'))
 		pics = glob.glob(os.path.join(f,'*.png'))
 		for i,p in enumerate(pics):
 			# print os.path.basename(p)
-			os.rename(p, os.path.join(os.path.dirname(p), folder_name+'_'+str(i)+'_mask.png'))
+			g_mask = cv2.imread(p, cv2.IMREAD_GRAYSCALE)
+			bin_mask = cv2.threshold(g_mask, 127, 255, cv2.THRESH_BINARY)[1]
+			# bin_mask.dtype = 'uint8'
+			# bin_mask = bin_mask > 0
+			# print bin_mask
+			# plt.imshow(bin_mask)
+			# plt.show()
+			cv2.imwrite(p, bin_mask)
+			# os.rename(p, os.path.join(os.path.dirname(p), folder_name+'_'+str(i)+'_mask.png'))
